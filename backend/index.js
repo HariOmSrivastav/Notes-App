c6ba552e75ab432c610f87e4c621cf3c907cb122
Hari Om Srivastav <anshsrivastav@172-11-9-167.lightspeed.chrlnc.sbcglobal.net>
2025-02-25 17:20:39 +0530

    Change

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json()) ;
 
 app.use(
     cors({
-        origin: ["https://notes-app-ten-livid.vercel.app/"],
+        origin: ["https://notes-app-three-umber.vercel.app/"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
