8d7abcfed9420ca4f37ca80ab2536dae40619b63
Hari Om Srivastav <anshsrivastav@172-11-9-167.lightspeed.chrlnc.sbcglobal.net>
2025-02-26 16:07:49 +0530

    C

--- frontend/notes-app/vite.config.js
+++ frontend/notes-app/vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Allows external access
+  },
+  preview: {
+    allowedHosts: ['*'], // Allow all hosts
+  }
 })
